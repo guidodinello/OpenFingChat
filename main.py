732bd647e6d9499e2f9ff81a45c98c78a69dcdb4
Guido Dinello <guido.dinello@gmail.com>
2024-05-20 16:49:06 -0300

    imports

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from store.data.models.subjects import SubjectModel
+# from store.data.models.subjects import SubjectModel
+
+from scrapper.scrapping_courses_info import hello
+
 # model = SubjectModel()
 
-id = SubjectModel.create("AGPI", "fklgdflgnlkdngkdfnglkdfngkdlfnxg")
-print("ID {id}")
+# id = SubjectModel.create("AGPI", "fklgdflgnlkdngkdfnglkdfngkdlfnxg")
+# print("ID {id}")
 
-s = SubjectModel.get(id)
-print(f"Retrieved subject: {s}")
+# s = SubjectModel.get(id)
+# print(f"Retrieved subject: {s}")
 
-all = SubjectModel.getAll()
-print(f"All subjects: {all}")
+# all = SubjectModel.getAll()
+# print(f"All subjects: {all}")
+
+hello()
