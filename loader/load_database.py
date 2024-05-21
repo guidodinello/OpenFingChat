bfc6a5126b70ac03d86fe06bfef20ac1cbdd85ab
Guido Dinello <guido.dinello@gmail.com>
2024-05-20 22:37:11 -0300

    Basic management functions for the vector database

---
+++ loader/load_database.py
@@ -0,0 +1,99 @@
+import json
+import logging
+from argparse import ArgumentParser
+from pathlib import Path
+
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from mock import MONGO_CLIENT
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+
+# capaz mover a un .env despues
+DB_PATH = "../vector_db"
+CORPUS_PATH = "../corpus"
+CACHE_MODELS_PATH = "../models/embeddings"
+
+
+def load_json(file_path):
+    # TODO: depende de como sea el formato de los json y si son muy grandes capaz es mejor hacerlo lazy
+    with open(file_path, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def load_vector_db(folder_path=DB_PATH):
+    cache_folder = Path(CACHE_MODELS_PATH)
+    cache_folder.mkdir(exist_ok=True)
+
+    model_name = "sentence-transformers/all-mpnet-base-v2"  # otro que podriamos probar "sentence-transformers/all-MiniLM-L6-v2"
+    embedding_model = HuggingFaceEmbeddings(
+        cache_folder=str(cache_folder),
+        model_name=model_name,
+        model_kwargs={"device": "cuda:0"},  # 'cpu' si no quieren/pueden GPU
+        show_progress=True,
+    )
+
+    if Path(folder_path + "/index.faiss").exists():
+        db = FAISS.load_local(
+            folder_path,
+            embeddings=embedding_model,
+            allow_dangerous_deserialization=True,
+        )
+    else:
+        db = seed_vector_db(folder_path, embedding_model=embedding_model)
+
+    return db
+
+
+def seed_vector_db(
+    db_folder_path=DB_PATH, data_folder_path=CORPUS_PATH, embedding_model=None
+):
+    all_docs_ids = MONGO_CLIENT["database"]["classes"].find({}, projection={"_id": 1})
+
+    for id in all_docs_ids:
+        class_transcript = Path(f"{data_folder_path}/{id}.json")
+        if not class_transcript.exists():
+            logging.error(f"File {class_transcript} not found!")
+            continue
+
+        content = load_json(class_transcript)
+        # capaz mover esto adentro del json loader y paralelizar
+        texts, metadatas = zip(
+            *(
+                (
+                    item["text"],
+                    {"class_id": id, "start": item["start"], "end": item["end"]},
+                )
+                for item in content
+            )
+        )
+
+        vectorstore = FAISS.from_texts(
+            texts=texts,
+            metadatas=metadatas,
+            embedding=embedding_model,
+        )
+
+    vectorstore.save_local(folder_path=db_folder_path)
+    return vectorstore
+
+
+def drop_vector_db(folder_path=DB_PATH):
+    folder_path = Path(folder_path)
+    (folder_path / "index.faiss").unlink(missing_ok=True)
+    (folder_path / "index.pkl").unlink(missing_ok=True)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--seed", action="store_true")
+    parser.add_argument("--drop", action="store_true")
+    args = parser.parse_args()
+
+    if args.seed:
+        logging.info(f"Seeding vector database from {DB_PATH}")
+        seed_vector_db()
+
+    if args.drop:
+        logging.info(f"Dropping vector database from {DB_PATH}")
+        drop_vector_db()
