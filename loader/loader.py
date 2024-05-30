b0391a8a7186529baf334f43b6c3629b023afb9f
Guido Dinello <guido.dinello@gmail.com>
2024-05-30 02:54:52 -0300

    loader script made callable from main

---
+++ loader/loader.py
@@ -0,0 +1,40 @@
+"""
+Usage:
+"""
+
+import logging
+from argparse import ArgumentParser
+
+from .vectorstore import VectorStore
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def main(args=None):
+    parser = ArgumentParser(prog="loader", allow_abbrev=False)
+    excl_group = parser.add_mutually_exclusive_group(required=True)
+    excl_group.add_argument(
+        "--seed",
+        action="store",
+        nargs="?",
+        const="",
+        help="Path to folder with data to seed the vector store",
+    )
+    excl_group.add_argument("--drop", action="store_true")
+    args = parser.parse_args(args)
+
+    store = VectorStore(seed_on_init=False)
+
+    if args.drop:
+        store.drop()
+
+    if (seed := args.seed) or seed == "":
+        store.seed(seed)
+
+
+if __name__ == "__main__":
+    import sys
+
+    main(sys.argv[1:])
