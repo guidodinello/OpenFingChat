21fc7702eeed72d44adce0d2e41433d4b612d222
Guido Dinello <guido.dinello@gmail.com>
2024-05-30 02:51:57 -0300

    update mock data

--- loader/mock.py
+++ loader/mock.py
@@ -2,18 +2,33 @@
 MONGO_CLIENT = {
     "database": {
         "classes": [
-            {"_id": 1, "name": "plogica01.json", "url": "plogica/01.mp4"},
-            {"_id": 2, "name": "plogica02.json", "url": "plogica/02.mp4"},
+            {
+                "_id": "ObjectId('665689bf97cf1d12f0260f43')",
+                "name": "Enrutamiento Interno: Introducción",
+                "url": "https://open.fing.edu.uy/courses/redes2/4/",
+                "video": "https://open.fing.edu.uy/media/redes2/redes2_04.mp4",
+                "transcribed": False,
+                "subjectId": "ObjectId('665689ba97cf1d12f0260f3b')",
+            },
+            {
+                "_id": "ObjectId('665689c097cf1d12f0260f45')",
+                "name": "OSPF - Clase 1",
+                "url": "https://open.fing.edu.uy/courses/redes2/5/",
+                "video": "https://open.fing.edu.uy/media/redes2/redes2_05.mp4",
+                "transcribed": False,
+                "subjectId": "ObjectId('665689ba97cf1d12f0260f3b')",
+            },
         ],
     }
 }
 
 
 class CustomList(list):
-    def find(self, query, projection=None):
-        if query == {}:
-            res = [doc["_id"] for doc in self]
-            print(res)
+    def find(self, filters=None, projection=None):
+        if filters == {"transcribed": True} and projection == {"_id": 1}:
+            # eliminar ObjectId()
+            format_id = lambda x: x.split("'")[1]
+            res = [format_id(doc["_id"]) for doc in self]
             return res
         else:
             pass
@@ -21,3 +36,10 @@ class CustomList(list):
 
 # Define a custom find method for the "classes" list
 MONGO_CLIENT["database"]["classes"] = CustomList(MONGO_CLIENT["database"]["classes"])
+
+# from mock import MONGO_CLIENT
+
+# lessons_id = MONGO_CLIENT["database"]["classes"].find(
+#     {"transcribed": True},
+#     projection={"_id": 1},
+# )
