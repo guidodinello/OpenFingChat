3162f8260a9bcdf7f2c79a7124bfaf31b0f185ff
Guido Dinello <guido.dinello@gmail.com>
2024-06-09 17:44:25 -0300

    add cuda detection for embedding model

--- loader/embeddings.py
+++ loader/embeddings.py
@@ -1,8 +1,10 @@
+import logging
 from os import PathLike
 from pathlib import Path
-from typing import Self
+from typing import Self  # precisa python>=3.11
 
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from torch import cuda
 
 
 class Embeddings:
@@ -17,7 +19,11 @@ class Embeddings:
     @staticmethod
     def _load_embedding_model(cache_path):
         cache_folder = Path(cache_path) / "embeddings"
-        cache_folder.mkdir(exist_ok=True, parents=True)
+        if not cache_folder.exists():
+            logging.info(f"Creating cache folder at {cache_folder}")
+            cache_folder.mkdir(exist_ok=True, parents=True)
+
+        device = "cuda:0" if cuda.is_available() else "cpu"
 
         model_name = "sentence-transformers/all-mpnet-base-v2"
         # otros que podriamos probar
@@ -26,6 +32,6 @@ class Embeddings:
         return HuggingFaceEmbeddings(
             cache_folder=str(cache_folder),
             model_name=model_name,
-            model_kwargs={"device": "cuda:0"},  # 'cpu' si no quieren/pueden GPU
+            model_kwargs={"device": device},
             show_progress=True,
         )
