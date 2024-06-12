8b2f798d53f0a49ad0464fb7eec24a621fb0f1bd
Josefina Guberna <jguberna96@gmail.com>
2024-06-12 20:32:43 -0300

    missing sources

--- chat/src/app/page.tsx
+++ chat/src/app/page.tsx
@@ -6,6 +6,7 @@ import { useChat } from "@/features/chat/context";
 import BottomSearch from "@/components/bottom-search";
 import MessagesList from "@/components/messages-list";
 import Empty from "@/components/empty";
+import Header from "@/components/header";
 
 const Home = () => {
   const {
@@ -14,9 +15,9 @@ const Home = () => {
 
   return (
     <Box height="100%" width="100%">
+      {messages.length > 0 && <Header />}
       {messages.length === 0 && <Empty />}
       {messages.length > 0 && <MessagesList />}
-
       <BottomSearch />
     </Box>
   );
