7b0915716a7674e697a88375ef745fb543cda475
Agustina Moraes <agustinamoraesn@gail.com>
2024-06-14 23:01:39 -0300

    rag with memory, andaba hasta antes de cambiar el modelo de embeddings

--- RAG/rag.py
+++ RAG/rag.py
@@ -2,11 +2,11 @@ import sys
 import os
 from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEndpoint
+from langchain.chains import create_history_aware_retriever
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain.memory import ChatMessageHistory
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.messages import HumanMessage
 from langsmith import traceable
 
 # Add parent directory to the sys.path (list of directories where Python is going to search for modules when doing imports)
@@ -14,7 +14,8 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-from RAG.prompt import TEMPLATE
+from RAG.prompt import PROMPT
+from RAG.contextualize_prompt import CONTEXTUALIZE_PROMPT
 from loader.vectorstore import VectorStore
 from store.data.models.subjects import SubjectModel
 from store.data.models.lessons import LessonModel
@@ -35,18 +36,38 @@ def initialize_llm():
         huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN,
     )
 
-def initialize_retriever():
+def initialize_retriever(llm):
     vectorstore = VectorStore()
     retriever = vectorstore.db.as_retriever()
-    return retriever
+    
+    # First we define a sub-chain that takes historical messages and the latest user question, and reformulates the question if it makes reference to any information in the historical information.
+    # Prompt to contextualize/reformulate the question to include history:
+    contextualize_q_system_prompt = CONTEXTUALIZE_PROMPT
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    # MessagesPlaceholder variable under the name "chat_history": pass in a list of Messages to the prompt that will be inserted after the system message and before the human message.
+    
+    history_aware_retriever = create_history_aware_retriever(llm, retriever, contextualize_q_prompt)
+    # create_history_aware_retriever: Creates a chain that takes conversation history and returns documents.
+    # Input keys: input and chat_history
+    # Output: has the same output schema as a retriever (returns documents)
+    # If there is no chat_history, then the input is just passed directly to the retriever.
+    # If there is chat_history, then the prompt and LLM will be used to generate the search query. That search query is then passed to the retriever.
+    
+    return history_aware_retriever
 
 def initialize_prompt():
     prompt = ChatPromptTemplate.from_messages([
-        ("system", TEMPLATE),
-        ("context", "{context}"),
-        MessagesPlaceholder(variable_name="history"),
-        ("human", "{question}"),
+        ("system", PROMPT),
+        MessagesPlaceholder(variable_name="chat_history"), # Prompt template that assumes variable is already list of messages.
+        ("human", "{input}"),
     ])
+
     return prompt
 
 def format_docs(docs):
@@ -62,42 +83,39 @@ def format_docs(docs):
     return context#, metadata
 
 @traceable
-def rag(query):
+def rag(query, chat_history):
     llm = initialize_llm()
 
-    retriever = initialize_retriever()
+    history_aware_retriever = initialize_retriever(llm)
 
     prompt = initialize_prompt()
 
-    print("defini el prompt")
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    # create_stuff_documents_chain: Creates a chain for passing a list of Documents (context) to a model.
+    # It generates a question_answer_chain.
+    # Input keys: context, chat_history, and input.
 
-    rag_chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()} # RunnablePassthrough() copies the user’s question. It is a runnable that behaves almost like the identity function, except that it can be configured to add additional keys to the output, if the input is a dict.
-        | prompt
-        | llm
-        | StrOutputParser() # converts any input into a string
-    )
-    
-    print("defini la rag_chain")
-    
-    demo_ephemeral_chat_history_for_chain = ChatMessageHistory()
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    # create_retrieval_chain: Creates retrieval chain that retrieves documents and then passes them on.
+    # Applies the history_aware_retriever and question_answer_chain in sequence, retaining intermediate outputs such as the retrieved context for convenience.
+    # Input keys: input and chat_history
+    # Output: input, chat_history, context, and answer
 
-    rag_chain_with_history = RunnableWithMessageHistory(
-        rag_chain,
-        lambda session_id: demo_ephemeral_chat_history_for_chain, # El front deberia pasar un session id (establecer uno al entrar a la pagina, y cada vez que se recarga, limpiar el chat y enviar un session id nuevo). Ver como borrar la historia cada vez que se recibe un session id nuevo
-        input_messages_key="question",
-        history_messages_key="history",
-    )
-
-    print("defini la rag_chain_with_history")
-
-    return rag_chain_with_history.invoke(
-        {"question": query},
-        {"configurable": {"session_id": "unused"}},
-    )#, metadata
+    return rag_chain.invoke({"input": query, "chat_history": chat_history}) #, metadata
 
 if __name__ == "__main__":
-    response = rag("Qué es la evolución natural?")
-    print(response)
-    response2 = rag("Cuál fue mi primera pregunta?")
-    print(response2)
+    chat_history = []
+
+    question = "Qué es la evolución natural?"
+    ai_msg_1 = rag(question, chat_history)
+    chat_history.extend([HumanMessage(content=question), ai_msg_1["answer"]])
+    
+    print('\nAnswer to first question: ', ai_msg_1["answer"])
+    print('\nchat_history: ', chat_history)
+
+    second_question = "Cuál fue mi primera pregunta?"
+    ai_msg_2 = rag(second_question, chat_history)
+    chat_history.extend([HumanMessage(content=second_question), ai_msg_2["answer"]])
+
+    print('\nAnswer to second question: ', ai_msg_2["answer"])
+    print('\nchat_history: ', chat_history)
