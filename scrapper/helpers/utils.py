12fc4469f72f27bdb948b559541feab706e1e0d4
Guido Dinello <guido.dinello@gmail.com>
2024-09-19 15:43:38 -0300

    scrapper module refactor

--- scrapper/helpers/utils.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-
-def getPageContent(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    return soup
