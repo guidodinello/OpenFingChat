22ad41eeb9fe634129d9167bdcd3bf257033a152
Guido Dinello <guido.dinello@gmail.com>
2024-09-19 15:55:30 -0300

    backend module formatting

--- backend/api.py
+++ backend/api.py
@@ -1,26 +1,19 @@
 import logging
 
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from langchain_core.messages import HumanMessage
 
 from RAG.rag import rag
-from store.data.models.lessons import LessonModel
+from store.mongo.models.lessons import LessonModel
 
 from .models import ChatResponse, Source, UserQuery
-
-TEST = False
-
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
-)
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://chat-mu-lemon.vercel.app/"],
+    allow_origins=["http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -32,13 +25,15 @@ def generate_id():
     return str(len(chats) + 1)
 
 
-@app.post("/query")
-def query(query: UserQuery) -> ChatResponse:
-    logging.debug(query)
-    input = query.query
-    conversation_id = query.conversation_id
+# TODO: async/concurrency
 
-    # TODO: segun lo poco que mire el front, el conversation_id es un string vacio la primera vez
+
+@app.post("/query")
+def query(user_query: UserQuery) -> ChatResponse:
+    logging.debug(user_query)
+    user_question = user_query.query
+    conversation_id = user_query.conversation_id
+
     if conversation_id == "":
         conversation_id = generate_id()
 
@@ -46,8 +41,9 @@ def query(query: UserQuery) -> ChatResponse:
         chats[conversation_id] = []
     chat_history = chats[conversation_id]
 
-    answer = rag(input, chat_history)
-    chat_history.extend([HumanMessage(content=input), answer["answer"]])
+    answer = rag(user_question, chat_history)
+    # TODO: HumanMessage handling shouldnt be here in the API
+    chat_history.extend([HumanMessage(content=user_question), answer["answer"]])
 
     lessons = LessonModel()
     sources = []
